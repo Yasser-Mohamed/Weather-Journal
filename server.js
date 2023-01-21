0f7a45091feef58aaf242a495b366dd19dba1437
Yasser_MR <97233731+Yasser-Mohamed@users.noreply.github.com>
2023-01-21 20:07:26 +0200

    basic

--- server.js
+++ server.js
@@ -11,24 +11,31 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
-// Setup empty JS object to act as endpoint for all routes
-const proData = {};
+// Setup empty JS array to act as endpoint for all routes
+const projectData = [];
 // Initialize the main project folder
 app.use(express.static("./website"));
 // Callback function to complete GET '/all'
 // GET Route
-app.get("/getAll", (request, response) => response.status(200).send(proData));
+app.get("/get", (request, response) => {
+  response.send(projectData);
+  projectData = [];
+});
 // GET Route
-app.post("/addAll", (request, response) => {
-    proData = request.body;
-    console.log(proData);
-    response.status(200).send(proData);
+app.post("/post", (request, response) => {
+    console.log(request.body);
+    newEnt ={
+      temp: request.body.temp,
+      date : request.body.date,
+      content : request.body.content
+    }
+    projectData.push(newEnt)
   });
 const port = 4000;
 const hostname = "localhost";
 // function to test the server 
 const list = () =>{
-   console.log(`Server running at http://${hostname}:${port}/`);
+   console.log(`Server running at http://${hostname}:${port}`);
 }
 // spin up the server
 app.listen(port, list);
