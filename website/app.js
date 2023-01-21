760566aff8403d9a118479bd794c4254e91a2566
Yasser_MR <97233731+Yasser-Mohamed@users.noreply.github.com>
2023-01-21 20:04:50 +0200

    mod

--- website/app.js
+++ website/app.js
@@ -1,39 +1,43 @@
-// const { json } = require("body-parser");
-
 /* Global Variables */
 const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
-const apiKey = "e7882d553d7b100bd08be571fe82e9be";
+const apiKey = ",&appid=e7882d553d7b100bd08be571fe82e9be";
 const zip = document.getElementById("zip");
 const feelings = document.getElementById("feelings");
 const generate = document.getElementById("generate");
 // Create a new date instance dynamically with JS
 const d = new Date();
 const newDate = `${d.getMonth()+1}.${d.getDate()}.${d.getFullYear()}`;
-// Event listener to add function to existing HTML DOM element
-document.getElementById("generate").addEventListener("click", async function weather(){
-    const url = `${baseURL}${zip.value}&appid=${apiKey}&units=metric`;
-    const weather = getTemep(url);
-    await sendData({
-        temp: weather.main.temp ,
-        Date : newDate,
-        feelings: feelings.value
+
+//Event listener to add function to existing HTML DOM element
+document.getElementById("generate").addEventListener("click",action);
+function action(el){
+    var insertZip = zip.value;
+    var feeling = feelings.value;
+    getWeather(baseURL,insertZip,apiKey).then((data)=>{
+        postData("/post",{
+            content : feeling,
+            date : newDate,
+            temp: data.main.temp
+        })
     });
-    getWeb()
-})
+    updateUI();
+}
 /* Function to GET Project Data */
-async function getTemep(url){
-    const request = await fetch(url);
+async function getWeather(baseURL,zip,api){
+    const response = await fetch(baseURL+zip+api);
     try{
-        const result = await request.json();
-        return result;
-    } catch(err){
-        console.log(err);
+        const data = response.json();
+        return data;
+    }
+    catch(err){
+        console.log("Error :"+err);
     }
 }
 /* Function to POST data */
-async function sendData(data){
-    const request = await fetch("/addAll",{
-        method:'POST',
+async function postData(url='',data={}){
+    console.log(data);
+    const response = await fetch(url,{
+        method:'post',
         credentials:'same-origin',
         headers:{
             'Content-Type':'application/json'
@@ -41,23 +45,24 @@ async function sendData(data){
         body: JSON.stringify(data)
     });
     try{
-        const result = await request.JSON();
-        return result;
-    } 
-    catch(err) {
-        console.log(err);
-    }
-}
-/* Function to GET Web API Data*/
-async function getWeb(){
-    const request = await fetch("/getAll");
-    try{
-        const result = await request.JSON();
-        document.getElementById("date").innerHTML=`today is ${result.Date}`
-        document.getElementById("temp").innerHTML=`temp today is ${result.temp}`
-        document.getElementById("content").innerHTML=`feelings  ${result.feelings}`
+        const newD = response.json();
+        console.log(newD);
+        return newD;
     }
     catch(err){
-        console.log(err);
+        console.log("Error :"+err);
+    }
+}
+// function to update UI 
+async function updateUI(){
+    const request =await fetch("/get");
+    try{
+        const allData = await request.json();
+        document.getElementById("date").innerHTML=`Date today is : ${newDate}`;
+        document.getElementById("temp").innerHTML=`temp today is : ${allData[0].temp}`;
+        document.getElementById("content").innerHTML=`feelings : ${feelings.value}`;
+    }
+    catch(err){
+        console.log("Error :"+err);
     }
 }
